468488f5e216828fda3a9c739431f199b0993a3d
Dursun-ali <99251332+Dursun-ali@users.noreply.github.com>
2023-07-05 21:44:43 +0300

    data getirildi düzenlendi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import axios from 'axios';
 import './App.css';
 import MainPage from './component/MainPage';
+import { useEffect, useState } from 'react';
 
 function App() {
-  //todo: dataya burdan istek at
+
+  const key='725774d6898e98531b9fab2b8bf94b31&query';
+  const query='New York'
+  // const[array,setArray]=useState();
+
+// const params = {
+//   access_key: '725774d6898e98531b9fab2b8bf94b31',
+//   query: 'New York'
+// }
+
+  
+  // useEffect(()=>{
+  //    axios.get('http://api.weatherstack.com/current?access_key=725774d6898e98531b9fab2b8bf94b31&query=trabzon')
+  //   .then(response=>setArray(response.data))
+  //   .catch(error => {
+  //         console.log(error);
+  //       });
+  // },[])
+  // console.log(array);
   return (
       <> 
       <MainPage/>
--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import '../css/MainPage.css'
 import PlaceIcon from '@mui/icons-material/Place';
 import TripOriginIcon from '@mui/icons-material/TripOrigin';
 import SearchIcon from '@mui/icons-material/Search';
+import axios from 'axios';
+
 const MainPage = () => {
+    const[array,setArray]=useState();
+    const[value,setValue]=useState()
+    function inpFunc(e) {
+        setValue(e.target.value)
+    }
+    function inpWrite() {
+        axios.get(`http://api.weatherstack.com/current?access_key=725774d6898e98531b9fab2b8bf94b31&query=${value}`)
+        .then(response=>setArray(response.data))
+        .catch(error => {
+              console.log(error);
+            });
+    }
+    useEffect(()=>{
+        axios.get(`http://api.weatherstack.com/current?access_key=725774d6898e98531b9fab2b8bf94b31&query=New York`)
+       .then(response=>setArray(response.data))
+       .catch(error => {
+             console.log(error);
+           });
+     },[])
+     console.log(array?.current.weather_descriptions[0]);
     return (
         <>
             <div className="main-container">
-            <div className="search-container">
+                <div className="search-container">
                     <div className="row">
                         <div className="col-sm-12">
                             <div className='search-box-wrapper'>
-                                <input className='search-inp' type="text" />
-                                <button className='search-btn'><SearchIcon/> Ara</button>
+                                    <input onChange={inpFunc} className='search-inp' type="text" />
+                                    <button  onClick={inpWrite} className='search-btn'><SearchIcon /> Ara</button>
                             </div>
                         </div>
                     </div>
@@ -30,48 +52,46 @@ const MainPage = () => {
                                     </p>
                                 </div>
                                 <div className='heat-section d-flex'>
-                                    <div className='first-write'>9</div>
-                                    <div>
+                                    <div className='first-write'>{array?.current.temperature}</div>
+                                    {/* <div>
                                         <TripOriginIcon className='second-write' />
-                                    </div>
-
-
+                                    </div> */}
                                     <div className='third-write'>
-                                        C
+                                        ℃
                                     </div>
                                 </div>
                                 <div className='weather-section'>
                                     <p>
-                                        Güneşli
+                                        {array?.current.weather_descriptions[0]}
                                     </p>
                                 </div>
                                 <div className='location-section'>
-                                    <PlaceIcon className='location-icon' /> <span className='location-span'>New york , ABD</span>
+                                    <PlaceIcon className='location-icon' /> <span className='location-span'>{array?.location.region}</span>
                                 </div>
                             </div>
                         </div>
                         <div className="col-sm-7">
-                                <div className="row">
-                                    <div className="col-sm-5">
-                                        <div className='weather-img-container'>
-                                        <img src="https://www.freeiconspng.com/thumbs/weather-icon-png/weather-icon-png-8.png" alt="" />
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-7">
-                                        <div className='info-wrapper'>
-                                        <ul className='info-ul' style={{listStyleType:"none"}}>
-                                            <li> <span className='info-span-first'>Rüzgar Hızı : </span> <span className='info-span-second'>10 km/h</span> </li>
-                                            <li><span className='info-span-first'>Rüzgar Yönü :</span> <span className='info-span-second'>Kuzey</span></li>
-                                            <li><span className='info-span-first'>Basınç :</span> <span className='info-span-second'>55 Pascal</span></li>
-                                            <li><span className='info-span-first'>Nem :</span> <span className='info-span-second'>77%</span></li>
-                                        </ul>
-                                        </div>
+                            <div className="row">
+                                <div className="col-sm-5">
+                                    <div className='weather-img-container'>
+                                        <img src={array?.current.weather_icons} alt="" />
                                     </div>
                                 </div>
+                                <div className="col-sm-7">
+                                    <div className='info-wrapper'>
+                                        <ul className='info-ul' style={{ listStyleType: "none" }}>
+                                            <li> <span className='info-span-first'>Rüzgar Hızı : </span> <span className='info-span-second'>{array?.current.wind_speed}  m/s</span> </li>
+                                            <li><span className='info-span-first'>Rüzgar Yönü :</span> <span className='info-span-second'>{array?.current.wind_dir}</span></li>
+                                            <li><span className='info-span-first'>Basınç :</span> <span className='info-span-second'>{array?.current.pressure} Pascal</span></li>
+                                            <li><span className='info-span-first'>Nem :</span> <span className='info-span-second'>{array?.current.humidity}%</span></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-        </div>
+            </div>
         </>
     );
 }
